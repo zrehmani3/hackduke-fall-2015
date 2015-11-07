7b8172b70c9e380b61efe5f03ce135a6f87b48bb
Siddu Duddikunta <siddu@siddu.me>
2015-11-07 16:36:49 -0500

    Initial java project

---
+++ server/src/main/java/org/hackduke/HelloWorldController.java
@@ -0,0 +1,15 @@
+package org.hackduke;
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/greeting")
+    public String greeting() {
+        return "Hello, World!";
+    }
+}
