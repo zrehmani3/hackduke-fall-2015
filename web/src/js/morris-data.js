bc26bfe2460f4f5327d1945f971cb528c22c099f
William Yang <yangzwilliam@gmail.com>
2015-11-07 21:14:58 -0500

    reduced tables.html down to just a table

--- web/src/js/morris-data.js
+++ web/src/js/morris-data.js
@@ -46,70 +46,19 @@ $(function() {
             period: '2012 Q1',
             iphone: 10687,
             ipad: 4460,
-            itouch: 2028
+            itouch: 2028,
+            iMac: 4000
         }, {
             period: '2012 Q2',
             iphone: 8432,
             ipad: 5713,
-            itouch: 1791
+            itouch: 1791,
+            iMac: 5000
         }],
         xkey: 'period',
         ykeys: ['iphone', 'ipad', 'itouch'],
         labels: ['iPhone', 'iPad', 'iPod Touch'],
         pointSize: 2,
-        hideHover: 'auto',
-        resize: true
-    });
-
-    Morris.Donut({
-        element: 'morris-donut-chart',
-        data: [{
-            label: "Download Sales",
-            value: 12
-        }, {
-            label: "In-Store Sales",
-            value: 30
-        }, {
-            label: "Mail-Order Sales",
-            value: 20
-        }],
-        resize: true
-    });
-
-    Morris.Bar({
-        element: 'morris-bar-chart',
-        data: [{
-            y: '2006',
-            a: 100,
-            b: 90
-        }, {
-            y: '2007',
-            a: 75,
-            b: 65
-        }, {
-            y: '2008',
-            a: 50,
-            b: 40
-        }, {
-            y: '2009',
-            a: 75,
-            b: 65
-        }, {
-            y: '2010',
-            a: 50,
-            b: 40
-        }, {
-            y: '2011',
-            a: 75,
-            b: 65
-        }, {
-            y: '2012',
-            a: 100,
-            b: 90
-        }],
-        xkey: 'y',
-        ykeys: ['a', 'b'],
-        labels: ['Series A', 'Series B'],
         hideHover: 'auto',
         resize: true
     });
